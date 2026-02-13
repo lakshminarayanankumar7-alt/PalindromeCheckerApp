3ee0af028ac1d2a7daf9c0ab936977e68ca42cc6
lakshminarayanankumar7-alt <lakshminarayanankumar7@gmail.com>
2026-02-13 10:52:47 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("welcme to palindrome checker management system");
+        System.out.println("version:1.0");
+        System.out.println("system initialized sucessfully");
+    }
+}
